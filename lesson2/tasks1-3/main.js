b5b88480e0b2c2e8c3edf6e1245f3a2a447d58d0
danyagithb <danil2019sinegub@gmail.com>
2025-02-07 13:24:00 +0100

    add folder 'lesson2' and completed 3 tasks

---
+++ lesson2/tasks1-3/main.js
@@ -0,0 +1,67 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let arr = [12, 34, 'arrays', false, true, ' ', 12, null, true, 'sar'];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title, pageCount, genre.
+
+let book1 = {
+    title: 'Robin',
+    pageCount: 1,
+    genre: 'roman'
+}
+
+let book2 = {
+    title: 'Island',
+    pageCount: 4,
+    genre: 'interes'
+}
+
+let book3 = {
+    title: 'All and all',
+    pageCount: 9,
+    genre: 'text'
+}
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+
+let book11 = {
+    title: 'Robin',
+    pageCount: 1,
+    genre: 'roman',
+    authors: [{name: 'Danya', age: 18}]
+}
+
+let book22 = {
+    title: 'Robin',
+    pageCount: 100,
+    genre: 'Text',
+    authors: [{name: 'Petya', age: 99}]
+}
+
+let book33 = {
+    title: 'Jordan',
+    pageCount: 164,
+    genre: 'roman',
+    authors: [{name: 'Mark', age: 38}]
+}
+
+
+
+
+
+
+
+
