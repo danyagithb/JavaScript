e2c3159b5eb25d6af7352ba9064c28f46eeb464a
danyagithb <danil2019sinegub@gmail.com>
2025-02-08 21:38:29 +0100

    add folder 'lesson3' and completed 7 tasks

---
+++ lesson3/tasks1-4/main.js
@@ -0,0 +1,47 @@
+// #y7crMeFwHcS
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+for ( i = 0; i < 10; i++ ) {
+    document.write(`
+        <div>TASK1</div>
+    `)
+}
+
+// #TYj7ncx
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for ( a = 0; a < 10; a++) {
+    document.write(`
+        <div>TASK(2) ${a}</div>
+    `)
+}
+
+
+// #uzkt71dp
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+let d = 0;
+while (d < 20) {
+    document.write(`
+        <h1>TASK3</h1>
+    `)
+    d++
+}
+
+
+
+// #OeT7t3uUMFi
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+
+let b = 0;
+while (b < 20) {
+    document.write(`
+        <h1>TASK(4) ${b}</h1>
+    `)
+    b++
+}
+
+
+
+
---
+++ lesson3/tasks5-7/main.js
@@ -0,0 +1,111 @@
+// #vLSZKMlO
+// - Використовуючи дані з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write(`<ul>`)
+for (let item of listOfItems) {
+
+    document.write(`
+        <li>${item}</li>
+   `)
+
+}
+
+document.write(`</ul>`)
+
+
+
+// #Hdjws7E
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (let product of products) {
+
+    document.write(`
+        <div class="product-card">
+             <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+             <img src="${product.image}" alt="" class="product-image">
+        </div>
+    `)
+
+}
+
+
+// #4WrHwFTEop0
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+
+for (let user of users) {
+
+    if (user.status === true) {
+        console.log(user)
+    }
+
+}
+
+console.log('')
+
+for (let user of users) {
+
+    if (user.status === false) {
+        console.log(user)
+    }
+
+}
+
+console.log('')
+
+
+for (let user of users) {
+
+    if (user.age > 30) {
+        console.log(user)
+    }
+
+}
+
+
+
+
+
+
+
